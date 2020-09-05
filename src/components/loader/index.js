999beac7f11ce1cca1690cf32f43b88be43e6ec1
zubair7384 <zubair.iqbal2018@gmail.com>
2020-09-05 13:37:56 +0500

    added loader

---
+++ src/components/loader/index.js
@@ -0,0 +1,35 @@
+import React from "react"
+import "./styles.scss"
+
+export default function Loader() {
+  return (
+    <div className="loader_container">
+      <div class="boxes">
+        <div class="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  )
+}
